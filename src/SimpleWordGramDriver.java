d1ba6b4633adf4abc2d98fffd55f53c65aaf9336
Sude Almus <kadriye.almus@duke.edu>
2020-09-14 21:41:35 -0500

    test

--- src/SimpleWordGramDriver.java
+++ src/SimpleWordGramDriver.java
@@ -1,5 +1,6 @@
 public class SimpleWordGramDriver {
     public static void main(String[] args) {
+        //me
         String[] words = {"Computer", "Science", "is", "fun"};
         WordGram g = new WordGram(words,0,4);
         System.out.printf("gram = %s, length = %d, hash = %d\n",
